a0b75542ca8d8684413ede13f02162db2d709381
ZainHanif88 <zain.hanif88@gmail.com>
2024-02-17 14:03:34 +0100

    Routing

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,35 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+        {title: "Catalog", path: "/catalog"},
+        {title: "About", path: "/about"},
+        {title: "Contact", path: "/contact"},
+
+]
+
+const rightLinks = [
+    {title: "Login", path: "/login"},
+    {title: "Register", path: "/register"},
+
+]
+
+const navStyles = {
+
+    color:'inherit', 
+    typography:'h6',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'grey.500',
+        textDecoration: 'none',
+        backgroundColor: 'transparent',
+    },
+    '&.active': {
+        textDecoration: 'none',
+        color: 'text.secondary',
+    }
+}
 
 interface Props{
     darkMode: boolean;
@@ -10,15 +41,56 @@ export default function Header({ darkMode, handleThemeChange }: Props){
 
     return(
         <AppBar position='static' sx={{mb:4}}>
-                <Toolbar>
-                    <Typography variant = 'h6'>
-                            RE-STORE
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                    <Typography
+                        variant='h6'
+                        component={NavLink}
+                        to='/'
+                        sx={navStyles}
+                    >
+                        RE-STORE
                     </Typography>
                     <Switch checked={darkMode} onChange={handleThemeChange} />
+                </Box>
+                    <List sx= {{display: 'flex'}}>
+                       {midLinks.map(({title, path}) => (
+                        <ListItem                   
+                           component={NavLink}
+                            to={path}
+                            key={path} 
+                            sx={navStyles}   
+                            >
+                            {title.toUpperCase()}
+                        </ListItem>
+                       ))}
+ 
+                    </List>
+                    
+                    <Box>
+                    <IconButton size='large' sx={{color:'inherit'}} >
+                        <Badge badgeContent={4} color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx= {{display: 'flex'}}>
+                       {rightLinks.map(({title, path}) => (
+                        <ListItem                   
+                           component={NavLink}
+                            to={path}
+                            key={path} 
+                            sx={navStyles}
+                            >
+                          
+                            {title.toUpperCase()}
+
+                        </ListItem>
+                       ))}
+ 
+                    </List>
+                    </Box>
                 </Toolbar>
         </AppBar>    
-
-
-
     )
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,11 +2,6 @@ import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
-
-
-
-
-
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
